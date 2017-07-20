5cb30238d14ac51efb721a816527d61fc740a1f2
pyraetos <pyraetos@pyraetos.net>
2017-07-20 12:57:18 -0400

    Signed-off-by: pyraetos <pyraetos@pyraetos.net>

--- src/net/pyraetos/pgenerate/ScorpionMap.java
+++ src/net/pyraetos/pgenerate/ScorpionMap.java
@@ -22,23 +22,24 @@ public class ScorpionMap extends MultiLayerMap {
 		int bG = (int)(84f * pB);
 		int bB = (int)(60f * pB);
 		double pG = 1d-pB;
-		int gR = (int)(206f * pG);
-		int gG = (int)(182f * pG);
-		int gB = (int)(173f * pG);
+		int gR = (int)(176f * pG);
+		int gG = (int)(162f * pG);
+		int gB = (int)(153f * pG);
 		return new Color(bR+gR,bG+gG,bB+gB).getRGB();
 	}
 	
 	@Override
 	public void layer(int layer){
 		if(layer > 4) return;
+		System.out.println("Starting layer " + layer);
 		layer();
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				switch(layer){
 				case(0): pixels[i * height + j] = getBrownGradient(j, i); break;
-				case(1): if(pg.getValue(i, j) > 1.) pixels[i * height + j] = OCP_LIGHT_GREEN.getRGB(); break;
-				case(2): if(pg.getValue(i, j) > 1.25) pixels[i * height + j] = OCP_GREEN.getRGB(); break;			
-				case(3): if(pg.getValue(i, j) > 1.5) pixels[i * height + j] = OCP_DARK_BROWN.getRGB(); break;			
+				case(1): if(pg.getValue(i, j) > .75) pixels[i * height + j] = OCP_LIGHT_GREEN.getRGB(); break;
+				case(2): if(pg.getValue(i, j) > 1) pixels[i * height + j] = OCP_GREEN.getRGB(); break;			
+				case(3): if(pg.getValue(i, j) > 1.35) pixels[i * height + j] = OCP_DARK_BROWN.getRGB(); break;			
 				case(4): if(pg.getValue(i, j) > 2) pixels[i * height + j] = OCP_GRAY.getRGB(); break;			
 				}
 			}
